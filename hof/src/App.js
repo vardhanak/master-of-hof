6704f56ab2057d923083d4377c98a2d357e6d86d
vardhanak <achantakrishnavardhan@gmail.com>
2021-12-29 13:40:46 +0530

    first commit

---
+++ hof/src/App.js
@@ -0,0 +1,12 @@
+import HigherOrderComponent from './component/HigherOrderComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderComponent/>
+    </div>
+  );
+}
+
+export default App;
